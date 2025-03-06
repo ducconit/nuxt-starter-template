37837037d9200254540f14ffdfbeb30f7a32eaaf
DNT <ducconit@gmail.com>
2025-03-06 22:57:28 +0700

    feat: add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+definePageMeta({
+  layout: 'auth',
+})
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen min-w-screen">
+    <Card class="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl">
+          Login
+        </CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="grid gap-4">
+          <div class="grid gap-2">
+            <Label for="email">Email</Label>
+            <Input id="email" type="email" placeholder="email@example.com" required />
+          </div>
+          <div class="grid gap-2">
+            <div class="flex items-center">
+              <Label for="password">Password</Label>
+              <a href="#" class="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </a>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" class="w-full">
+            Login
+          </Button>
+        </div>
+        <div class="mt-4 text-center text-sm">
+          Don't have an account?
+          <NuxtLink to="/register" class="underline">
+            Sign up
+          </NuxtLink>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+</template>
