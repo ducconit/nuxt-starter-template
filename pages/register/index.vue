f2d0c9d11d5f35b9a3f6e1f7c89e088e0c501431
DNT <ducconit@gmail.com>
2025-03-06 22:57:19 +0700

    feat: add register page

---
+++ pages/register/index.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+definePageMeta({
+    layout: 'auth',
+})
+</script>
+
+<template>
+    <div class="flex items-center justify-center min-w-screen min-h-screen">
+        <Card class="mx-auto max-w-sm">
+            <CardHeader>
+                <CardTitle class="text-xl">
+                    Sign Up
+                </CardTitle>
+                <CardDescription>
+                    Enter your information to create an account
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div class="grid gap-4">
+                    <div class="grid grid-cols-2 gap-4">
+                        <div class="grid gap-2">
+                            <Label for="first-name">First name</Label>
+                            <Input id="first-name" placeholder="Max" required />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label for="last-name">Last name</Label>
+                            <Input id="last-name" placeholder="Robinson" required />
+                        </div>
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="email">Email</Label>
+                        <Input id="email" type="email" placeholder="email@example.com" required />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="password">Password</Label>
+                        <Input id="password" type="password" />
+                    </div>
+                    <Button type="submit" class="w-full">
+                        Create an account
+                    </Button>
+                </div>
+                <div class="mt-4 text-center text-sm">
+                    Already have an account?
+                    <NuxtLink to="/login" class="underline">
+                        Sign in
+                    </NuxtLink>
+                </div>
+            </CardContent>
+        </Card>
+    </div>
+</template>
